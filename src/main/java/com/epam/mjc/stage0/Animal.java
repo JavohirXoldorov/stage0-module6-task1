1c3d9d11f32864cc07569020f94b9c6ddb44e13e
JavohirXoldorov <javohirxoldorov7@gmail.com>
2022-08-05 09:17:42 +0500

    testing

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,36 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFour;
 
+    public Animal(String color, int numberOfPaws, boolean hasFour) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFour = hasFour;
+    }
+
+    public String getDescription() {
+        String desc = "This animal is mostly " + color + ". It has " + numberOfPaws + " ";
+        if (numberOfPaws == 1) desc += "paw ";
+        else desc += "paws ";
+        desc += "and ";
+        if (hasFour) desc += "a fur.";
+        else desc += "no fur.";
+
+        return desc;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+
+    public boolean isHasFour() {
+        return hasFour;
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,13 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
 
+    public Bird() {
+        super("blue", 2, false);
+    }
+
+    @Override
+    public String getDescription() {
+        return "This animal is mostly blue. It has " + getColor() + " paws and no fur. Moreover, it has " + getNumberOfPaws() + " wings and can fly.";
+    }
 }
